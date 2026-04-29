8256b045ec7c963e20c1fa12e892f2be7bf3ca35
Mahdi <mahdigholipour@outlook.com>
2026-04-29 13:06:29 +0330

    fix(notifications): improve readability by adding line breaks in conditional statements

--- cronbot/NoticationsService.php
+++ cronbot/NoticationsService.php
@@ -33,27 +33,36 @@ class ServiceMonitor
     public function RunNotifactions()
     {
         $invoices = $this->getActiveInvoices();
-        if ($invoices == false) return;
+        if ($invoices == false)
+            return;
         foreach ($invoices as $invoice) {
             if ($invoice['time_cron'] != null) {
                 $time_cron = time() - $invoice['time_cron'];
-                if ($time_cron < 1600) continue;
+                if ($time_cron < 1600)
+                    continue;
             }
             update("invoice", "time_cron", time(), "id_invoice", $invoice['id_invoice']);
             $check_send = json_decode($invoice['notifctions'], true);
             $data = $this->processInvoice($invoice);
-            if (!is_array($data)) continue;
+            if (!is_array($data))
+                continue;
             $result = false;
             if (!$check_send['volume']) {
-                if ($this->status_cron['volume']) $result = $this->checkVolumeThreshold($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
+                if ($this->status_cron['volume'])
+                    $result = $this->checkVolumeThreshold($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
             }
-            if ($result) $data['invoice'] = select("invoice", "*", "id_invoice", $invoice['id_invoice']);
+            if ($result)
+                $data['invoice'] = select("invoice", "*", "id_invoice", $invoice['id_invoice']);
             if (!$check_send['time']) {
-                if ($this->status_cron['day']) $this->checkTimeExpiration($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
+                if ($this->status_cron['day'])
+                    $this->checkTimeExpiration($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
             }
-            if ($this->status_cron['remove']) $this->shouldRemoveService($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
-            if ($this->status_cron['remove_volume']) $this->shouldRemoveServiceـvolume($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
-            if ($data['panel']['inboundstatus'] == "oninbounddisable" && $data['panel']['type'] == "marzban") $this->active_inbound_expire($data['invoice'],  $data['userData'], $data['panel']);
+            if ($this->status_cron['remove'])
+                $this->shouldRemoveService($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
+            if ($this->status_cron['remove_volume'])
+                $this->shouldRemoveServiceـvolume($data['invoice'], $data['user'], $data['userData'], $invoice['username']);
+            if ($data['panel']['inboundstatus'] == "oninbounddisable" && $data['panel']['type'] == "marzban")
+                $this->active_inbound_expire($data['invoice'], $data['userData'], $data['panel']);
         }
     }
 
@@ -73,15 +82,20 @@ class ServiceMonitor
 
         // Get panel information
         $panelInfo = select("marzban_panel", "*", "name_panel", $invoice['Service_location'], "select");
-        if (!$panelInfo) return false;
+        if (!$panelInfo)
+            return false;
 
+        if ($panelInfo['status'] == "disabled")
+            return false;
         // Get user information
         $user = select("user", "*", "id", $invoice['id_user'], "select");
-        if ($user == false) return false;
+        if ($user == false)
+            return false;
 
         // Get username data from panel
         $userData = $this->Panel->DataUser($invoice['Service_location'], $username);
-        if (!$userData || $userData['status'] == "Unsuccessful") return;
+        if (!$userData || $userData['status'] == "Unsuccessful")
+            return;
         return [
             'invoice' => $invoice,
             'panel' => $panelInfo,
@@ -113,11 +127,12 @@ class ServiceMonitor
     }
     private function shouldRemoveService($invoice, $user, $userData, $username)
     {
-        if (!in_array($userData['status'], ['limited', 'expired'])) return false;
+        if (!in_array($userData['status'], ['limited', 'expired']))
+            return false;
         $timeService = $userData['expire'] - time();
         $daysRemaining = intval($timeService / 86400);
         $removalThreshold = intval("-" . $this->setting['removedayc']);
-        $result =  $daysRemaining <= $removalThreshold;
+        $result = $daysRemaining <= $removalThreshold;
         $statusText = $statusMap = [
             'active' => $this->textBotLang['users']['stateus']['active'],
             'limited' => $this->textBotLang['users']['stateus']['limited'],
@@ -132,29 +147,35 @@ class ServiceMonitor
             $this->Panel->RemoveUser($invoice['Service_location'], $username);
             $message = "📌 کاربر گرامی بدلیل عدم تمدید، سرویس {$invoice['username']} از لیست سرویس های شما حذف گردید\n\n🌟 جهت تهیه سرویس جدید از بخش خرید سرویس اقدام فرمایید";
             $reportMessage = "📌 اطلاعیه کرون حذف\n\nنام کاربری سرویس :‌ <code>{$invoice['username']}</code>\nوضعیت سرویس : $statusText\nتعداد روز باقی مانده ‌:‌$daysRemaining\nحجم باقی مانده : $remainingVolume";
-            $this->send_notifactions($invoice, $user, $message, false,$invoice['bottype']);
+            $this->send_notifactions($invoice, $user, $message, false, $invoice['bottype']);
             $this->sendReportNotification($reportMessage);
         }
     }
     private function shouldRemoveServiceـvolume($invoice, $user, $userData, $username)
     {
-        if (!in_array($userData['status'], ['limited', 'expired'])) return false;
+        if (!in_array($userData['status'], ['limited', 'expired']))
+            return false;
         $panel = select("marzban_panel", "*", "name_panel", $invoice['Service_location'], "select");
-        if ($panel['type'] != "marzban") return;
-        if ($userData['data_limit_reset'] != "no_reset") return;
-        if ($userData['status'] == "Unsuccessful") return;
-        if (in_array($userData['status'], ['Unknown', 'active', 'on_hold', 'disabled', 'expired'])) return;
+        if ($panel['type'] != "marzban")
+            return;
+        if ($userData['data_limit_reset'] != "no_reset")
+            return;
+        if ($userData['status'] == "Unsuccessful")
+            return;
+        if (in_array($userData['status'], ['Unknown', 'active', 'on_hold', 'disabled', 'expired']))
+            return;
         if (empty($userData['online_at']) or $userData['online_at'] == null) {
             $timelastconect = 0;
         } else {
             $time = strtotime($userData['online_at']);
             $timelastconect = (time() - $time) / 86400;
         }
-        if($timelastconect == 0)return;
+        if ($timelastconect == 0)
+            return;
         $timeService = $userData['expire'] - time();
         $daysRemaining = intval($timeService / 86400);
         $removalThreshold = intval($this->setting['cronvolumere']);
-        $result =  $timelastconect >= $removalThreshold;
+        $result = $timelastconect >= $removalThreshold;
         $statusText = [
             'active' => $this->textBotLang['users']['stateus']['active'],
             'limited' => $this->textBotLang['users']['stateus']['limited'],
@@ -177,13 +198,15 @@ class ServiceMonitor
     }
     private function active_inbound_expire($invoice, $userData, $panel_info)
     {
-        if ($invoice['uuid'] != null || $userData['data_limit_reset'] != "no_reset") return;
+        if ($invoice['uuid'] != null || $userData['data_limit_reset'] != "no_reset")
+            return;
         $inbound = explode("*", $panel_info['inbound_deactive']);
         update("invoice", "uuid", json_encode($userData['uuid']), "id_invoice", $invoice['id_invoice']);
         $proxies = [];
-        $proxies[$inbound[0]] = new stdClass();;
+        $proxies[$inbound[0]] = new stdClass();
+        ;
         $inbounds[$inbound[0]][] = $inbound[1];
-        $configs  = array(
+        $configs = array(
             "proxies" => $proxies,
             "inbounds" => $inbounds
         );
@@ -192,7 +215,8 @@ class ServiceMonitor
     private function checkTimeExpiration($invoice, $user, $userData, $username)
     {
         $validStatuses = ['expired', 'on_hold', 'limited'];
-        if (in_array($userData['status'], $validStatuses)) return;
+        if (in_array($userData['status'], $validStatuses))
+            return;
         $timeRemaining = $userData['expire'] - time();
         $daysRemaining = intval($timeRemaining / self::SECONDS_PER_DAY);
         $warningThreshold = intval($this->setting['daywarn']) * self::SECONDS_PER_DAY;
@@ -208,7 +232,7 @@ class ServiceMonitor
                 "نام کاربری سرویس :‌ <code>{$invoice['username']}</code>\n" .
                 "وضعیت سرویس : {$userData['status']}\n" .
                 "تعداد روز باقی مانده ‌:‌{$daysRemaining}";
-            $this->send_notifactions($invoice, $user, $message, true,$invoice['bottype']);
+            $this->send_notifactions($invoice, $user, $message, true, $invoice['bottype']);
             $this->sendReportNotification($reportMessage);
             $this->updateInvoiceStatus("time", $invoice);
             return true;
@@ -217,7 +241,8 @@ class ServiceMonitor
 
     private function send_notifactions($invoice, $status_cron_user, $message, $keyboard_active, $bot_token)
     {
-        if (intval($status_cron_user) == 0) return;
+        if (intval($status_cron_user) == 0)
+            return;
         $keyboard = $this->createExtendServiceKeyboard($invoice['id_invoice']);
         $keyboard = $keyboard_active ? $keyboard : null;
         sendmessage($invoice['id_user'], $message, $keyboard, 'HTML', $bot_token);
@@ -236,7 +261,8 @@ class ServiceMonitor
 
     private function sendReportNotification($reportMessage)
     {
-        if (empty($this->setting['Channel_Report'])) return;
+        if (empty($this->setting['Channel_Report']))
+            return;
 
 
         telegram('sendmessage', [
